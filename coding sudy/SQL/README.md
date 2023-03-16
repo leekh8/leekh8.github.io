faee6a377c54dbc4da35a8d634b9613f085e5860
leekh <amysun125@gmail.com>
2023-03-16 15:50:27 +0900

    update

--- coding sudy/SQL/README.md
+++ coding sudy/SQL/README.md
@@ -1,1 +1,2 @@
 [광주인공지능사관학교](https://gj-aischool.or.kr/) 의 pre-track 중 공부 내용을 정리
+[좀좀따리 문법 정리](./grammer.sql) 에 기본 문법 정리
---
+++ coding sudy/SQL/grammer.sql
@@ -0,0 +1,51 @@
+-- EMP 테이블의 구조
+
+-- emp_no: 직원 번호: 정수
+-- birth_dat: 생일: 날짜
+-- first_name: 성: 문자열
+-- last_name: 이름: 문자열
+-- gender: 성별: 문자열
+-- salary: 연봉: 정수
+-- from_date: 입사일: 날짜
+
+
+-- EMP 테이블의 구조 출력
+DESC EMP;
+
+
+-- EMP 테이블 조회
+SELECT *
+FROM EMP;
+
+
+-- EMP 테이블의 이름 조회
+SELECT first_name, last_name
+FROM EMP;
+
+
+-- 중복 없는 데이터 조회
+-- DISTINCT: 중복 제거
+SELECT DISTINCT emp_no
+FROM EMP;
+
+
+-- 조건에 맞는 정보 조회
+-- 성이 Lee인 직원의 모든 정보 조회
+SELECT *
+FROM EMP
+WHERE first_name = 'Lee';
+
+-- 성이 Lee이고 성별이 여자인 직원의 모든 정보 조회
+SELECT *
+FROM EMP
+WHERE first_name = 'Lee' AND gender = 'F';
+
+-- 범위를 지정해 검색
+-- BETWEEN: 범위 지정
+-- 연봉이 2000에서 3000 사이인 직원의 모든 정보 조회
+SELECT *
+FROM EMP
+WHERE (salary >= 2000) and (salary <= 3000);
+SELECT *
+FROM EMP
+WHERE salary BETWEEN 2000 AND 3000;
