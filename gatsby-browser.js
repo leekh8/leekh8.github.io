6928f53d552718c2906a8a7dddf7bc2a35ead4a1
leekh8 <amysun125@gmail.com>
2024-06-26 10:44:21 +0900

    feat: load mermaid script for browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,17 @@
+const React = require("react")
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="mermaid-js"
+      src="https://cdnjs.cloudflare.com/ajax/libs/mermaid/8.11.0/mermaid.min.js"
+      onLoad={() => {
+        if (window.mermaid) {
+          window.mermaid.initialize({ startOnLoad: true })
+        }
+      }}
+    />,
+  ])
+}
+
 require("katex/dist/katex.min.css")
