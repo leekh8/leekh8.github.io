4f6a2fe8f0c1a69f217aaad09dafb8bc5fc3ca7a
leekh8 <amysun125@gmail.com>
2024-09-23 09:33:31 +0900

    feat: Adding Structured Data to SEO Components as JSON-LD

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -2,7 +2,36 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { siteUrl } from "../../../blog-config"
 
-const SEO = ({ title, description, url }) => {
+const SEO = ({ title, description, url, author }) => {
+  const today = new Date().toISOString().split("T")[0]
+
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": url,
+    },
+    headline: title,
+    description: description,
+    author: {
+      "@type": "Person",
+      name: author,
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "leekh8's Blog",
+      logo: {
+        "@type": "ImageObject",
+        url: `${siteUrl}/profile.png`,
+      },
+    },
+    image: `${siteUrl}/profile.png`, // og image
+    url: url,
+    datePublished: today,
+    dateModified: today,
+  }
+
   return (
     <Helmet>
       <title>{title}</title>
@@ -11,6 +40,11 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+
+      {/* JSON-LD structured data */}
+      <script type="application/ld+json">
+        {JSON.stringify(structuredData)}
+      </script>
     </Helmet>
   )
 }
