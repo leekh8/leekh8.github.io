5adcd1d4b76858972132f9928285e0ee6a47632e
leekh8 <amysun125@gmail.com>
2025-05-16 19:54:06 +0900

    fix: Prevent static build error by importing missing author from blog-config in pages/about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import Layout from "components/Layout"
 import SEO from "components/SEO"
-import { title, description, siteUrl } from "../../blog-config"
+import { title, author, siteUrl } from "../../blog-config"
 
 const Content = styled.div`
   padding: 2rem;
