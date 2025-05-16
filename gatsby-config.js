41e190e91a4923ca8f522134f93a029664ec65d4
leekh8 <amysun125@gmail.com>
2025-05-16 20:01:36 +0900

    fix: Remove pathPrefix to align sitemap URLs with GitHub Pages root path

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const blogConfig = require("./blog-config")
 const { title, description, author, siteUrl } = blogConfig
 
 module.exports = {
-  pathPrefix: "/haiblog",
+  // pathPrefix: "/haiblog",
   siteMetadata: {
     title: title,
     description: description,
