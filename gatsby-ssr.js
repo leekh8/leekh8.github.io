3eb53c83b9eed064c3fa315ed6cf5bb30805014e
khlee <khlee@withnetworks.com>
2026-04-09 10:32:30 +0900

    fix: gatsby-ssr.js 중복 onRenderBody 병합, lang=ko 추가, 불필요한 Mermaid CDN 제거
    - onRenderBody 이중 선언으로 AdSense 스크립트 미삽입 버그 수정
    - html lang=ko 설정 (Accessibility 점수 개선)
    - Mermaid CDN CSS/JS 제거 (gatsby-remark-mermaid SSR 처리로 불필요, 성능 개선)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,46 +4,28 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
 const React = require("react")
 const config = require("./blog-config")
 
-exports.onRenderBody = ({ setHeadComponents }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+  // html lang 설정 (Accessibility + SEO)
+  setHtmlAttributes({ lang: "ko" })
+
   setHeadComponents([
-    <title key="title">{config.title}</title>, // 페이지의 <title> 태그 설정
-    <meta key="description" name="description" content={config.description} />, // 페이지 설명 설정
-    <meta key="og-title" property="og:title" content={config.title} />, // Open Graph 타이틀 설정
+    <meta key="description" name="description" content={config.description} />,
+    <meta key="og-title" property="og:title" content={config.title} />,
     <meta
       key="og-description"
       property="og:description"
       content={config.description}
-    />, // Open Graph 설명 설정
-    <meta key="og-url" property="og:url" content={config.siteUrl} />, // Open Graph URL 설정
-
-    // Mermaid.js의 CSS 파일을 외부 CDN에서 불러옴
-    <link
-      key="mermaid-css"
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/mermaid/8.11.0/mermaid.min.css"
     />,
-
-    // <!-- Google AdSense Script -->
-    // eslint-disable-next-line react/jsx-key
+    <meta key="og-url" property="og:url" content={config.siteUrl} />,
+    // Google AdSense
     <script
+      key="adsense"
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3742032449453805"
       crossOrigin="anonymous"
-    ></script>,
+    />,
   ])
-}
-
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  if (typeof window !== "undefined") {
-    setPostBodyComponents([
-      <script
-        key="mermaid"
-        src="https://unpkg.com/mermaid/dist/mermaid.min.js"
-      ></script>,
-    ])
-  }
 }
